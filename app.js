09b5f7eb1f0a528bac820499a152e262310e7a44
johame72 <johnhmerkel@gmail.com>
2024-02-15 21:42:22 -0600

    x

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import postsRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/posts', postsRoutes);
+app.use('/user', userRoutes);
 
 export default app;
